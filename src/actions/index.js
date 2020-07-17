fa984848561ee0d3650a31324608cc710dc8d8c1
Deepak Bhari <deepak.bhari@lifts.to>
2020-07-17 18:28:46 +0100

    redux basic flow

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+import api from '../apis/index';
+
+export const fetchPosts = () => {
+
+    return async (dispatch, getState) => {
+        // const response = await mockApi();
+
+        const response = await api.get('/posts');
+
+        dispatch ({
+            type: 'FETCH_POSTS',
+            payload: response.data
+        });
+    }
+}
+
+// const mockApi = () =>{
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve({
+//                 data: [1,2,3,4,5]
+//             });
+//         }, 2000)
+//     });
+// }
---
+++ src/components/PostList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { fetchPosts } from '../actions/index';
+
+class PostList extends React.Component {
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
+    render() {
+        console.log(this.props.posts);
+        
+        return (
+            <div>PostList</div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    { fetchPosts }
+)(PostList);
