2751254e71aad8c4c6abb6922a32a5fc1c97d086
Deepak Bhari <deepak.bhari@lifts.to>
2020-07-19 01:35:42 +0100

    Memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import api from '../apis/index';
 
 export const fetchPosts = () => {
@@ -14,16 +16,36 @@ export const fetchPosts = () => {
     }
 }
 
-export const fetchUser = (id) => {
-    return async (dispatch, getState) => {
-        const response = await api.get(`/users/${id}`);
+// export const fetchUser = (id) => {
+//     return async (dispatch, getState) => {
+//         const response = await api.get(`/users/${id}`);
         
-        dispatch({
-            type: 'FETCH_USER',
-            payload: response.data
-        });
+//         dispatch({
+//             type: 'FETCH_USER',
+//             payload: response.data
+//         });
+//     }
+// }
+
+
+export const fetchUser = (id) => {
+    return (dispatch) => {
+        _fetchUser(id, dispatch);
     }
 }
+
+// export const fetchUser = id => dispatch => {
+//     _fetchUser(id, dispatch);
+// }
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await api.get(`/users/${id}`);
+    
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+});
 
 // const mockApi = () =>{
 //     return new Promise((resolve, reject) => {
